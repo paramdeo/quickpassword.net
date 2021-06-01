2ec37bb018659f8041b64620122b2264af968dbc
Paramdeo Singh <paramdeo@paramdeo.com>
2021-06-01 13:57:22 -0400

    Updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,14 +7,7 @@ module.exports = {
   darkMode: false,
   theme: {
     fontFamily: {
-      sans: ['-apple-system', 'BlinkMacSystemFont', '"Segoe UI"', 'Helvetica', 'Arial', 'sans-serif', "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"],
       mono: ['SFMono-Regular', 'Consolas', 'Liberation Mono', 'Menlo', 'monospace']
-    },
-    colors: {
-      'qp-1': '#2B2D42',
-      'qp-2': '#EDF2F4',
-      'qp-3': '#8D99AE',
-      'qp-4': '#ff7f51',
     },
     extend: {},
   },
