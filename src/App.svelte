fc99782330f47cbb2f70929e08d02025fc397801
Paramdeo Singh <paramdeo@paramdeo.com>
2022-06-17 07:25:17 -0400

    Updates

--- src/App.svelte
+++ src/App.svelte
@@ -54,26 +54,26 @@
 
       <label for="basic-url" class="form-label fw-bold">24 Characters</label>
       <div class="input-group mb-5">
-        <input class="form-control form-control-lg user-select-none" type="text" bind:value={pw.chars24} disabled readonly><!-- Svelte binds this value so when the password is regenerated, the displayed value is also updated -->
+        <input class="form-control form-control-lg user-select-none mono" type="text" bind:value={pw.chars24} disabled readonly><!-- Svelte binds this value so when the password is regenerated, the displayed value is also updated -->
         <button class="btn btn-danger text-uppercase fw-bold" type="button" on:click={ navigator.clipboard.writeText(pw.chars24.replaceAll('&amp;', '&')).then(copied) }>{@html copy}</button>
       </div>
 
       <label for="basic-url" class="form-label fw-bold">36 Characters</label>
       <div class="input-group mb-5">
-        <input class="form-control form-control-lg user-select-none" type="text" bind:value={pw.chars36} disabled readonly>
+        <input class="form-control form-control-lg user-select-none mono" type="text" bind:value={pw.chars36} disabled readonly>
         <button class="btn btn-danger text-uppercase fw-bold" type="button" on:click={ navigator.clipboard.writeText(pw.chars36.replaceAll('&amp;', '&')).then(copied) }>{@html copy}</button>
       </div>
 
       <label for="basic-url" class="form-label fw-bold">48 Characters</label>
       <div class="input-group mb-5">
-        <input class="form-control form-control-lg" type="text" bind:value={pw.chars48} disabled readonly>
+        <input class="form-control form-control-lg user-select-none mono" type="text" bind:value={pw.chars48} disabled readonly>
         <button class="btn btn-danger text-uppercase fw-bold" type="button" on:click={ navigator.clipboard.writeText(pw.chars48.replaceAll('&amp;', '&')).then(copied) }>{@html copy}</button>
       </div>
 
       <div class="d-grid gap-2">
         <button class="btn btn-danger fs-5 fw-bold" type="button" on:click={ () => { 
           populate(); }}>Generate Passwords</button>
-        <p class="mt-3 text-center fs-6">Copyright &copy; <a href="https://paramdeo.com" target="_blank" title="Personal website of Paramdeo Singh">Paramdeo Singh</a> <strong>·</strong> Made with ☕️ in Guyana <strong>·</strong> <a href="https://github.com/paramdeo/quickpassword.net" target="_blank" rel="noopener" title="Source on GitHub"><svg class="inline" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16"><path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"/></svg></a></p>
+        <p class="mt-3 text-center fs-6">Copyright &copy; <a href="https://paramdeo.com" target="_blank" title="Personal website of Paramdeo Singh">Paramdeo Singh</a> <strong>·</strong> Made with ☕️ in Guyana <strong>·</strong> <a href="https://github.com/paramdeo/quickpassword.net" target="_blank" rel="noopener" title="Source on GitHub">GitHub</a></p>
       </div>
     
     </div>
